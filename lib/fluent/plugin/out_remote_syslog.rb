409cde892d5ffd1b14368d61ee20e3d3f93003df
Richard Lee <dlackty@gmail.com>
2014-09-22 23:24:01 +0800

    Add test for output plugin

--- lib/fluent/plugin/out_remote_syslog.rb
+++ lib/fluent/plugin/out_remote_syslog.rb
@@ -1,45 +1,47 @@
 require "fluent/mixin/config_placeholders"
 
-class RemoteSyslogOutput < Fluent::Output
-  Fluent::Plugin.register_output("remote_syslog", self)
+module Fluent
+  class RemoteSyslogOutput < Fluent::Output
+    Fluent::Plugin.register_output("remote_syslog", self)
 
-  config_param :hostname, :string, :default => ""
+    config_param :hostname, :string, :default => ""
 
-  config_param :key_name, :string, :default => "message"
+    config_param :key_name, :string, :default => "message"
 
-  config_param :remote_hostname, :string
-  config_param :port, :integer, :default => 25
+    config_param :remote_hostname, :string
+    config_param :port, :integer, :default => 25
 
-  config_param :facility, :string, :default => "user"
-  config_param :severity, :string, :default => "notice"
-  config_param :tag, :string, :default => "fluentd"
+    config_param :facility, :string, :default => "user"
+    config_param :severity, :string, :default => "notice"
+    config_param :tag, :string, :default => "fluentd"
 
-  include Fluent::Mixin::ConfigPlaceholders
+    include Fluent::Mixin::ConfigPlaceholders
 
-  def initialize
-    super
-    require "remote_syslog_logger"
-  end
+    def initialize
+      super
+      require "remote_syslog_logger"
+    end
 
-  def configure(conf)
-    super
-    @logger = RemoteSyslogLogger::UdpSender.new(@remote_hostname,
-                                                @port,
-                                                facility: @facility,
-                                                severity: @severity,
-                                                program: @tag,
-                                                local_hostname: @hostname)
-  end
+    def configure(conf)
+      super
+      @logger = RemoteSyslogLogger::UdpSender.new(@remote_hostname,
+                                                  @port,
+                                                  facility: @facility,
+                                                  severity: @severity,
+                                                  program: @tag,
+                                                  local_hostname: @hostname)
+    end
 
-  def shutdown
-    super
-    @logger.close if @logger
-  end
+    def shutdown
+      super
+      @logger.close if @logger
+    end
 
-  def emit(tag, es, chain)
-    chain.next
-    es.each do |time, record|
-      @logger.transmit record[@key_name]
+    def emit(tag, es, chain)
+      chain.next
+      es.each do |time, record|
+        @logger.transmit record[@key_name]
+      end
     end
   end
 end
---
+++ test/plugin/out_remote_syslog.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+require "fluent/plugin/out_remote_syslog"
+
+class RemoteSyslogOutputTest < Test::Unit::TestCase
+  def setup
+    super
+    Fluent::Test.setup
+  end
+
+  CONFIG = %[
+    type remote_syslog
+    hostname foo.com
+    remote_hostname example.com
+    port 5566
+    severity debug
+    tag testunit
+  ]
+
+  def create_driver(conf = CONFIG)
+    Fluent::Test::OutputTestDriver.new(Fluent::RemoteSyslogOutput) {}.configure(conf)
+  end
+
+  def test_configure
+    d = create_driver
+    logger = d.instance.instance_variable_get(:@logger)
+
+    assert_not_nil logger
+    assert_equal "example.com", logger.instance_variable_get(:@remote_hostname)
+    assert_equal 5566, logger.instance_variable_get(:@remote_port)
+    p = logger.instance_variable_get(:@packet)
+    assert_equal "foo.com", p.hostname
+    assert_equal 1, p.facility
+    assert_equal "testunit", p.tag
+    assert_equal 7, p.severity
+  end
+end
